c07183fe081acf4584e38dd7dbb3e6634984ffd8
talhahzahid <talhahzahid415@gmail.com>
2025-01-14 17:40:20 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import mainbanner from "/public/hero_iphone16_avail__euwzls69btea_large.jpg";
 const App = () => {
   return (
     <div className="">
+
+      {/* navbar */}
       <div className="flex justify-center gap-10 items-center p-4 bg-[#171718] text-white">
         <div>
           <h1 className="text-xl">
@@ -35,8 +37,54 @@ const App = () => {
           </h1>
         </div>
       </div>
-<img src={mainbanner1} alt="" />
-<img src={mainbanner} alt="" />
+{/* main 1  */}
+      <div className="relative w-full h-[90vh]">
+        <img
+          src={mainbanner1}
+          alt="IPhone 13 Pro Max"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 flex justify-center mt-[3rem] text-center text-white px-4">
+          <div className="space-y-3">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-shadow-lg">
+              IPhone 13 Pro Max
+            </h1>
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-light text-shadow-lg">
+              Hello, Apple Intelligence
+            </h2>
+            <button className="border border-[#0071e3] p-2 w-[7rem] bg-[#0071e3] m-3 rounded-2xl">
+              Learn More
+            </button>
+            <button className="border border-[#0071e3] hover:bg-[#0071e3] p-2 w-[4rem] m-3 rounded-2xl">
+              Buy
+            </button>
+          </div>
+        </div>
+      </div>
+    {/* main 2 */}
+      <div className="relative w-full h-[90vh]">
+        <img src={mainbanner} alt="" className=" w-full h-full object-cover " />
+        <div className="absolute inset-0 flex justify-center text-center mt-[3rem] text-white">
+          <div className="space-y-3">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-shadow-lg">
+              IPhone 12 Pro Max
+            </h1>
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-light text-shadow-lg">
+              Hello, Apple Intelligence
+            </h2>
+            <button className="border border-[#0071e3] p-2 w-[7rem] bg-[#0071e3] m-3 rounded-2xl">
+              Learn More
+            </button>
+            <button className="border border-[#0071e3] hover:bg-[#0071e3] p-2 w-[4rem] m-3 rounded-2xl">
+              Buy
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+
+      
     </div>
   );
 };
